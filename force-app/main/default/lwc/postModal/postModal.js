2346a35ea24d1a4c03c32bec059930fc5e738b65
DOS <anton.datskovskiy@gmail.com>
2022-11-12 00:12:24 +0200

    Lesson 7 fix

--- force-app/main/default/lwc/postModal/postModal.js
+++ force-app/main/default/lwc/postModal/postModal.js
@@ -1,12 +1,10 @@
-import { api } from 'lwc';
 import LightningModal from 'lightning/modal';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import createPost from '@salesforce/apex/PostSaveController.createPost';
 
 export default class PostModal extends LightningModal {
-    //@api content;
     externalId;
-    userld;
+    userId;
     title;
     body;
 
@@ -14,26 +12,22 @@ export default class PostModal extends LightningModal {
         this.close('cancel');
     }
 
-    externalIdChange(event) {
-        this.externalId = event.target.value;
-    }
-
-    userIdChange(event) {
-        this.userld = event.target.value;
-    }
-
-    titleChange(event) {
-        this.title = event.target.value;
-    }
-
-    bodyChange(event) {
-        this.body = event.target.value;
+    postHandleChange(event) {
+        if (event.target.name == 'externalId') {
+            this.externalId = event.target.value;    
+        } else if (event.target.name == 'userId') {
+            this.userId = event.target.value;    
+        } else if (event.target.name == 'title') {
+            this.title = event.target.value;    
+        } else if (event.target.name == 'body') {
+            this.body = event.target.value;    
+        }      
     }
 
     createPostClick() {
         const post = {
             externalId: this.externalId,
-            userId: this.userld,
+            userId: this.userId,
             title: this.title,
             body: this.body
         };
@@ -54,7 +48,7 @@ export default class PostModal extends LightningModal {
                 this.dispatchEvent(
                     new ShowToastEvent({
                         title: 'Error creating record',
-                        message: '',
+                        message: error.message,
                         variant: 'error',
                     }),
                 );
